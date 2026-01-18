d6cb75dbf03be4d7c3d769798f378cd28a69a0a6
whonore <wolfhonore@gmail.com>
2026-01-17 17:09:09 -0800

    feat: day 10 part 1

--- flake.nix
+++ flake.nix
@@ -7,5 +7,13 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShells.default = pkgs.mkShell { packages = [ pkgs.picat ]; }; });
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.picat
+            # mip solver
+            pkgs.glpk
+          ];
+        };
+      });
 }
